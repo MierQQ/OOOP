da5efd06bae83f7d16b2ab912ebe465a874fe85a
MierQQ <57473179+MierQQ@users.noreply.github.com>
2020-09-06 21:14:36 +0900

    Update main.cpp
    Изменение в соответствии требованию запуска программы

--- Lab0b/src/main.cpp
+++ Lab0b/src/main.cpp
@@ -1,8 +1,15 @@
 #include "Analyzer.h"
+#include <iostream>
 
-int main() {
-	Analyzer analyzer;
-	analyzer.AddFile("input.txt");
-	analyzer.AddFile("input2.txt");
-	analyzer.OutCSV("output.csv");
+int  main(int argc, char* argv[]){
+	if (argc > 1) {
+		Analyzer analyzer;
+		for (int i = 1; i < argc - 1; ++i) {
+			analyzer.AddFile(argv[i]);
+		}
+		analyzer.OutCSV(argv[argc - 1]);
+	}
+	else {
+		std::cerr << "No arguments" << std::endl;
+	}
 }
